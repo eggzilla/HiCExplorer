da558ac3f15cf120e739732d2a6435960f1b80d5
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-06-16 21:05:35 +0200

    Iteration implemented, but fails

--- hicexplorer/hicReduceNoise.py
+++ hicexplorer/hicReduceNoise.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import normalize
 
+
 def parse_arguments(args=None):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -44,7 +45,9 @@ class ReduceNoise():
         distribution = np.zeros(hicMatrix.matrix.shape[0])
         instances, features = hicMatrix.matrix.nonzero()
         for i, j in zip(instances.tolist(), features.tolist()):
-            distribution[abs(i - j)] += hicMatrix.matrix[i, j]
+            if i - j < 0:
+                continue
+            distribution[i - j] += hicMatrix.matrix[i, j]
         plt.yscale('log')
         plt.xscale('log')
         plt.xlabel("Genomic distances")
@@ -56,7 +59,7 @@ class ReduceNoise():
         threshold_variance = 0.0008
         threshold_abs_mean = 80
         power = -3
-        for i in range(len(distribution) - window_size):
+        for i in xrange(0, len(distribution) - window_size, window_size):
             # compute mean
             # compute variance
             # if variance is too big, skip
@@ -65,49 +68,66 @@ class ReduceNoise():
             variance_normalized = np.var(normalized_values)
             if variance_normalized > threshold_variance and mean_absolute > threshold_abs_mean:
                 continue
-            
+
             # set all genomic distances within window size to absolute mean value
             values = []
-            index_values_row = []
-            index_values_column = []
-            
-            
-            for j in range(i, i+window_size):
-                for k in range():
+            # index_values_row = []
+            # index_values_column = []
+            print("sliding window iteration: ", i)
+            for j in range(i, i + window_size):
+                for k, l in zip(range(0, len(distribution)), range(j, len(distribution))):
                     # TODO get correct values
-                    value = 9
-                    values.append(value)
-                
+                    if hicMatrix.matrix[k, l] != 0:
+                        values.append(hicMatrix.matrix[k, l])
+
                 mean_interaction_count_difference = abs(mean_absolute - distribution[i])
                 change_values = [0] * len(values)
                 if mean_absolute < distribution[i]:
                     # rich-getting-richer
-                    for j in range(len(values)):
-                        change_values[j] = ((1-(values[j] / distribution[i] )) ** power) * mean_interaction_count_difference
-                    
+                    for k in range(len(values)):
+                        change_values[k] = ((1 - (values[k] / distribution[i])) ** power) * mean_interaction_count_difference
                 else:
                     # poor-getting-poorer
-                    for j in range(len(values)):
-                        change_values[j] = (((values[j] / distribution[i] )) ** power) * mean_interaction_count_difference
+                    for k in range(len(values)):
+                        change_values[k] = (((values[k] / distribution[i])) ** power) * mean_interaction_count_difference
                 # normalize to range [0, mean_interaction_count_difference]
                 min_value = min(change_values)
                 max_value = max(change_values)
-                for j in range(len(change_values)):
-                    change_values[j] = mean_interaction_count_difference * ((change_values[j] - min_value) / (max_value - min_value))
-                
-                if mean_absolute >= distribution[i]:
-                    change_values = change_values * -1
-                for j in range(len(values)):
-                    values[j] += int(np.around(change_values))
+                for k in range(len(change_values)):
+                    change_values[k] = mean_interaction_count_difference * ((change_values[k] - min_value) / (max_value - min_value))
+                    if np.isnan(change_values[k]):
+                        change_values[k] = 0
+                if mean_absolute < distribution[i]:
+                    for k in range(len(values)):
+                        values[k] += int(change_values[k])
+                else:
+                    for k in range(len(values)):
+                        values[k] -= int(change_values[k])
+                        if values[k] < 0:
+                            values[k] = 0
                 # TODO: write data back to matrix
 
+                values.reverse()
+                for k, l in zip(range(0, len(distribution)), range(j, len(distribution))):
+                    # TODO get correct values
+                    if hicMatrix.matrix[k, l] != 0:
+                        hicMatrix.matrix[k, l] = values.pop()
+                values = []
+                change_values = []
+        distribution = np.zeros(hicMatrix.matrix.shape[0])
+        instances, features = hicMatrix.matrix.nonzero()
+        for i, j in zip(instances.tolist(), features.tolist()):
+            if i - j < 0:
+                continue
+            distribution[abs(i - j)] += hicMatrix.matrix[i, j]
+        plt.yscale('log')
+        plt.xscale('log')
+        plt.xlabel("Genomic distances")
+        plt.ylabel("Number of interactions")
+        plt.plot(list(range(len(distribution))), distribution)
+        plt.savefig("interactions per genomic distance_corrected")
 
-
-            # print("mean_absolute", mean_absolute)
-            # print("variance_normalized", variance_normalized)
-            # print("values:",  distribution[i:i + window_size])
-            # print("normalized:", normalized_values)
-
+        hicMatrix.save("corrected.h5")
 
 if __name__ == '__main__':
     reduceNoise = ReduceNoise()
