a40d1fa19c3c3b37e557d304ef51aca4a8ec3d26
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-06-29 18:54:44 +0000

    Writing of data successful

--- hicexplorer/cpp/h5Interface.cpp
+++ hicexplorer/cpp/h5Interface.cpp
@@ -107,6 +107,7 @@ void H5Interface::writeMatrix(std::unordered_map<uint32_t, std::vector<matrixEle
     int32_t xOld = -1;
     for (auto it = pGenomicDistanceMap->begin(); it != pGenomicDistanceMap->end(); ++it) {
         for (auto itVector = (it->second)->begin(); itVector != (it->second)->end(); ++itVector) {
+            // std::cout << (*itVector).data << std::endl;
             if ((*itVector).data <= pRemoveLowInteractionCount) {
                 continue;
             }
@@ -125,6 +126,7 @@ void H5Interface::writeMatrix(std::unordered_map<uint32_t, std::vector<matrixEle
     int32_t* indices = &indicesVector[0];
     int64_t* data = &dataVector[0];
 
+    
     // create new h5 file
     H5::H5File* file = new H5::H5File(mMatrixPathOutput, H5F_ACC_TRUNC);
     // create group '/matrix'
@@ -133,26 +135,35 @@ void H5Interface::writeMatrix(std::unordered_map<uint32_t, std::vector<matrixEle
     // create 3 dataspaces: /matrix/indptr, /matrix/indices, /matrix/data
     int RANK = 1;
     // indptr
-    H5::DataSpace* dataspaceIndptr = new H5::DataSpace(RANK, indptr);
+    hsize_t dims[1];
+    dims[0] = indptrVector.size();
+    H5::DataSpace* dataspaceIndptr = new H5::DataSpace(RANK, dims);
     // create dataset
-    H5::DataSet* datasetIndptr = new H5::DataSet(file->createDataSet("/matrix/indptr"), H5::PredType::NATIVE_INT32,
-                                                *dataspaceIndptr);
+    H5::DataSet* datasetIndptr = new H5::DataSet(file->createDataSet("/matrix/indptr", H5::PredType::NATIVE_INT32,
+                                                *dataspaceIndptr));
+    datasetIndptr->write(indptr, H5::PredType::NATIVE_INT32);
     delete dataspaceIndptr;
     delete datasetIndptr;
 
     // indices
-    H5::DataSpace* dataspaceIndices = new H5::DataSpace(RANK, indices);
+    dims[0] = indicesVector.size();
+    
+    H5::DataSpace* dataspaceIndices = new H5::DataSpace(RANK, dims);
     // create dataset
-    H5::DataSet* datasetIndices r = new H5::DataSet(file->createDataSet("/matrix/indices"), H5::PredType::NATIVE_INT32,
-                                                *dataspaceIndices);
+    H5::DataSet* datasetIndices = new H5::DataSet(file->createDataSet("/matrix/indices", H5::PredType::NATIVE_INT32,
+                                                *dataspaceIndices));
+    datasetIndices->write(indices, H5::PredType::NATIVE_INT32);
     delete dataspaceIndices;
     delete datasetIndices;
 
     // data
-    H5::DataSpace* dataspaceData = new H5::DataSpace(RANK, data);
+    dims[0] = dataVector.size();
+    
+    H5::DataSpace* dataspaceData = new H5::DataSpace(RANK, dims);
     // create dataset
-    H5::DataSet* datasetData = new H5::DataSet(file->createDataSet("/matrix/data"), H5::PredType::NATIVE_INT64,
-                                                *dataspaceData);
+    H5::DataSet* datasetData = new H5::DataSet(file->createDataSet("/matrix/data", H5::PredType::NATIVE_INT64,
+                                                *dataspaceData));
+    datasetData->write(data, H5::PredType::NATIVE_INT64);
     delete dataspaceData;
     delete datasetData;
 
@@ -160,17 +171,17 @@ void H5Interface::writeMatrix(std::unordered_map<uint32_t, std::vector<matrixEle
     // copy old content of /intervals
     // create group intervals
     // create dataspaces for 
-    H5::H5File* fileOld = new H5::H5File( mMatrixPath, H5F_ACC_RDONLY );
-    H5::Group* groupIntervalsOld = new H5::Group (file->openGroup("intervals"));
+    // H5::H5File* fileOld = new H5::H5File( mMatrixPath, H5F_ACC_RDONLY );
+    // H5::Group* groupIntervalsOld = new H5::Group (file->openGroup("intervals"));
 
-    H5::Group* groupIntervalsCopy = new H5::Group(groupIntervalsOld);
-    H5::Group* groupIntervalsNew = new H5::Group(file->createGroup(groupIntervalsNew->getId()));
+    // H5::Group* groupIntervalsCopy = new H5::Group(groupIntervalsOld);
+    // H5::Group* groupIntervalsNew = new H5::Group(file->createGroup(groupIntervalsNew->getId()));
 
     
-    delete groupIntervalsNew;
-    delete groupIntervalsCopy;
-    delete groupIntervalsOld;
-    delete fileOld;
+    // delete groupIntervalsNew;
+    // delete groupIntervalsCopy;
+    // delete groupIntervalsOld;
+    // delete fileOld;
     delete file;
     
     return;
