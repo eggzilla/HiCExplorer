72b38a2b56f9716560422e1036f5ee75fc7ca7b7
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-10-16 20:01:04 +0200

    Continue work

--- hicexplorer/hicDifferentialAnalysis.py
+++ hicexplorer/hicDifferentialAnalysis.py
@@ -26,10 +26,9 @@ from matplotlib.ticker import FixedLocator
 
 def parse_arguments(args=None):
 
-    heatmap_parser = heatmap_options()
+    # heatmap_parser = heatmap_options()
 
     parser = argparse.ArgumentParser(
-        parents=[heatmap_parser],
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description='Computes differentally analysis based on the comparison of the found tad domains.')
 
@@ -37,7 +36,7 @@ def parse_arguments(args=None):
     parser.add_argument('--tadFiles', '-m',
                         help='Tad score files to compute the differntial expression of the chromatine.',
                         nargs=2,
-                        type=argparse.FileType('r'),
+                        type=str,
                         required=True,
                         metavar='BED file')
 
@@ -59,8 +58,15 @@ def parse_arguments(args=None):
 def main(args=None):
     """This differentally analysis is based on the comparison of the found tad domains."""
     args = parse_arguments().parse_args(args)
+
+    # check for same bin size
+    # accept only if match
+
+
     tad_domains = []
+    print args.tadFiles
     for tadFile in args.tadFiles:
+        print tadFile
         # tad_domains[tadFile] = []
         intervals = []
         with open(tadFile, 'r') as tadFile_:
@@ -71,34 +77,58 @@ def main(args=None):
         tad_domains.append(intervals)
         intervals = None
     
+    hicMatrix = hm.hiCMatrix()
     interval_tree = [None] * len(tad_domains)
     i = 0
     while i < len(interval_tree):
-        interval_tree[i], _ = intervalListToIntervalTree(tad_domains[i])
+        interval_tree[i], _ = hicMatrix.intervalListToIntervalTree(tad_domains[i])
         i += 1
     i = None
 
     result = {}
     # print()
+    print("interval_tree", len(interval_tree))
+    print("interval_tree", len(interval_tree[0]))
+    print("interval_tree", len(interval_tree[1]))
+    
     # compute intersection of interval trees per chromosome
     for chrom in interval_tree[0]:
-        for tree in interval_tree[1:]:
-            if chrom in tree:
-                # print('tree', tree)
-                if chrom in result:
-                    
-                    result[chrom] &= tree[chrom]
-                else:
-                    result[chrom] = interval_tree[0][chrom] & tree[chrom] 
+        i = 1
+        print("chrom", chrom)
+        while i < len(interval_tree) and chrom in interval_tree[i]:
+            print("chrom_inner")
+            
+            if chrom in result:
+                print('chrom seen, create intersection: ', chrom)
+                result[chrom] &= interval_tree[i][chrom]
             else:
-                result[chrom] = None
-                break
+                print('chrom new, create intersection: ', chrom)
+                print('interval_tree[0][chrom]', interval_tree[0][chrom])
+                print('interval_tree[i][chrom]', interval_tree[i][chrom])
+                
+                result[chrom] = interval_tree[0][chrom] & interval_tree[i][chrom] 
+            i += 1
+        if i != len(interval_tree):
+            result[chrom] = None
+        
 
-    
+        # for tree in interval_tree[1:]:
+        #     if chrom in tree:
+        #         # print('tree', tree)
+        #         if chrom in result:
+                    
+        #             result[chrom] &= tree[chrom]
+        #         else:
+        #             result[chrom] = interval_tree[0][chrom] & tree[chrom] 
+        #     else:
+        #         result[chrom] = None
+        #         break
+
+    print result
     # return tads which occure in all files
 
-    with open(args.outFileName, 'w') as outFile:
-        for chrom in result:
-            if result[chrom] is not None:
-                for interval in result[chrom]:
-                    outFile.write("{}\t{}\t{}\t{}".format(chrom, interval[0], interval[1]))
+    # with open(args.outFileName, 'w') as outFile:
+    #     for chrom in result:
+    #         if result[chrom] is not None:
+    #             for interval in result[chrom]:
+    #                 outFile.write("{}\t{}\t{}\t{}".format(chrom, interval[0], interval[1]))
